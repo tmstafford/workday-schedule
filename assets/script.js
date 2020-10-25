a58306aa8312c0f66b07b37d6b65082cc26ff4b1
tmstafford <tatumstafford1@gmail.com>
2020-10-25 11:47:56 -0500

    create and load tasks functions

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,30 @@
 // add current date
 $("#currentDay").text(moment().format('dddd') + ", " + moment().format('MMMM Do'));
 
+let tasks = {};
+
+// function to load tasks to page from storage or make tasks empty
+// then create a task
+let loadTasks = function() {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+    if (!tasks) {
+        tasks = {};
+    };
+
+    createTask(tasks);
+};
+
+// creating the task p element for each hour, append to the hour div 
+let createTask = function() {
+    $.each(tasks, function(hour, arr) {
+        let taskP = $("<p>")
+        .addClass("description task-" + hour)
+        .text(arr);
+
+        $("#task-item-" + hour).append(taskP);
+    })
+};
+
+
+
+loadTasks();
